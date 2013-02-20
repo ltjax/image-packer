63e12f522717c341c5a6b53e63282cfc05cc827b
marius.elvert <marius.elvert@78444f7d-101d-4965-ba13-78dd9c210362>
2013-02-20 22:11:13 +0000

    9patch support

--- source/ImagePacker.cpp
+++ source/ImagePacker.cpp
@@ -9,7 +9,10 @@
 #include <boost/filesystem/path.hpp>
 #include <boost/filesystem/convenience.hpp>
 #include <boost/filesystem/fstream.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/optional.hpp>
 #include <replay/pixbuf_io.hpp>
+#include <tuple>
 
 struct ImageEntryType
 {
@@ -21,35 +24,101 @@ struct ImageEntryType
 	
 	bool					IsNinePatch;
 	
-	replay::vector2<int>	XCenter;
-	replay::vector2<int>	XContents;
-	
-	replay::vector2<int>	YCenter;
-	replay::vector2<int>	YContents;
+	replay::box<int>		ScaleableArea;
+	replay::box<int>		FillArea;
 };
+
+std::tuple<unsigned int, unsigned int>
+AnalyzeLine(replay::pixbuf const& Image, unsigned int Offset, unsigned int Axis)
+{
+	replay::vector2i Coord(0);
+	Coord[Axis^1]=Offset;
+
+	replay::vector2i Delta(0);
+	Delta[Axis]=1;
+
+	auto w=Image.get_width(), h=Image.get_height();
+	while (Coord[0]<w&&Coord[1]<h&&Image.get_pixel(Coord[0], Coord[1])[3]==0)
+		Coord+=Delta;
+
+	// Mark the beginning of the black line
+	unsigned int Begin=Coord[Axis];
+
+	unsigned char Black[]={0,0,0,255};
+	while (Coord[0]<w&&Coord[1]<h&&std::equal(Black, Black+4, Image.get_pixel(Coord[0], Coord[1])))
+		Coord+=Delta;
+
+	// Mark the end of the black line
+	unsigned int End=Coord[Axis];
+
+	while (Coord[0]<w&&Coord[1]<h&&Image.get_pixel(Coord[0], Coord[1])[3]==0)
+		Coord+=Delta;
+
+	// Check if we reached the end
+	if (Coord[0]!=w && Coord[1]!=h)
+		throw std::invalid_argument("Invalid black-line size specifier.");
+
+	return std::make_tuple(Begin, End);
+}
+
+void AddFile(std::vector<ImageEntryType>& List, boost::filesystem::path const& FilePath, boost::filesystem::path const& RelativeFilePath)
+{
+	// Check if this is a supported image format
+	if (extension(FilePath)!=".png")
+		return;
+
+	std::cout << "Loading " << FilePath.string() << std::endl;
+		
+	auto Image=replay::pixbuf_io::load_from_file(FilePath);
+	ImageEntryType Entry;
+	Entry.RelativePath=RelativeFilePath;
+
+	// Check if this is a 9-patch
+	if (boost::algorithm::ends_with(FilePath.leaf().string(), ".9.png"))
+	{
+		auto w=Image->get_width(), h=Image->get_height();
+		if (Image->get_channels() != 4)
+			throw std::invalid_argument("9-patch must have alpha channel");
+
+		if (w < 4 || h < 4)
+			throw std::invalid_argument("9-patch images must be at least 4x4");
+
+		// Analyze top and left
+		auto ScalableX=AnalyzeLine(*Image, h-1, 0);
+		auto ScalableY=AnalyzeLine(*Image, 0, 1);
+		
+		// Analyze bottom and right
+		auto FillX=AnalyzeLine(*Image, 0, 0);
+		auto FillY=AnalyzeLine(*Image, w-1, 1);
+
+		if (std::get<0>(FillX)==std::get<1>(FillX))
+			FillX=std::make_tuple(0, w-2);
+
+		if (std::get<0>(FillY)==std::get<1>(FillY))
+			FillY=std::make_tuple(0, h-2);
+		
+		Entry.ScaleableArea.set(std::get<0>(ScalableX), std::get<0>(ScalableY), std::get<1>(ScalableX), std::get<1>(ScalableY));
+		Entry.FillArea.set(std::get<0>(FillX), std::get<0>(FillY), std::get<1>(FillX), std::get<1>(FillY));
+
+		// Extract the actual image data
+		Entry.Image=Image->get_sub_image(1, 1, w-2, h-2);
+
+		Entry.IsNinePatch=true;
+	}
+	else
+	{
+		Entry.Image=Image;
+	}
+		
+		
+	List.push_back(Entry);
+}
 
 void ScanFile(std::vector<ImageEntryType>& List, boost::filesystem::path const& Path)
 {
-	auto AddFile=[&](boost::filesystem::path const& File, boost::filesystem::path const& Relative)
-	{
-		if (extension(File)!=".png")
-			return;
-		
-		std::cout << "Loading " << File.string() << std::endl;
-		
-		auto Image=replay::pixbuf_io::load_from_file(File);
-		
-		ImageEntryType Entry;
-		Entry.Image=Image;
-		Entry.RelativePath=Relative;
-		
-		List.push_back(Entry);
-		
-	};
-	
 	if (!is_directory(Path))
 	{
-		AddFile(Path, Path.leaf());
+		AddFile(List, Path, Path.leaf());
 		return;
 	}
 	
@@ -74,7 +143,7 @@ void ScanFile(std::vector<ImageEntryType>& List, boost::filesystem::path const& 
 				Relative /= *j;
 		
 		if (!Relative.empty())
-			AddFile(Path/Relative, Relative);
+			AddFile(List, Path/Relative, Relative);
 	}
 }
 
@@ -142,31 +211,78 @@ void PackImages(boost::filesystem::path const& ResultImage, std::vector<ImageEnt
 	replay::pixbuf_io::save_to_png_file(File, *Result, 9);
 }
 
-void WriteDictionary(std::ofstream& File, const std::string& DictionaryName, std::vector<ImageEntryType> const& List)
+void WriteBox(std::ofstream& File, replay::box<int> const& Box)
+{
+	File << "x=" << Box.left <<
+		", y=" << Box.bottom <<
+		", w=" << Box.get_width() <<
+		", h=" << Box.get_height();
+}
+
+void WriteDictionary(std::ofstream& File, const std::string& DictionaryName, const std::string& NinePatchDir, std::vector<ImageEntryType> const& List)
 {
 	File << "local " << DictionaryName << "= {\n";
+
+	std::size_t NinePatchCount=0;
 	
 	for (auto i=List.begin(); i!=List.end(); ++i)
 	{
 		auto&& Box=i->Box;
 
+		if (i->IsNinePatch)
+		{
+			++NinePatchCount;
+			continue;
+		}
+
 		// Remove the file extension
 		boost::filesystem::path NameOnly=i->RelativePath;
 		NameOnly.replace_extension();
 
-		File << "  [\"" << NameOnly.string() << "\"]={Box={"
-			<< "x=" << Box.left <<
-			", y=" << Box.bottom <<
-			", w=" << Box.get_width() <<
-			", h=" << Box.get_height() << "}}";
+		File << "\t[\"" << NameOnly.string();
+		File << "\"]={Box={";
+		WriteBox(File, Box);
+		File << "}}";
 		
 		if (i+1 != List.end())
 			File << ",\n";
 		else
 			File << "\n";
 	}
-	
 	File << "}\n";
+	
+	if (NinePatchCount)
+	{
+		File << "\nlocal " << NinePatchDir << "= {\n";
+		for (auto i=List.begin(); i!=List.end(); ++i)
+		{
+			auto&& Box=i->Box;
+
+			if (!i->IsNinePatch)
+				continue;
+
+			// Remove the file extension
+			boost::filesystem::path NameOnly=i->RelativePath;
+			NameOnly.replace_extension();
+			NameOnly.replace_extension(); // twice for the ".9"
+		
+
+			File << "\t[\"" << NameOnly.string();
+			File << "\"]={Box={";
+			WriteBox(File, Box);
+			File << "}, Scalable={";
+			WriteBox(File, i->ScaleableArea);
+			File << "}, Fill={";
+			WriteBox(File, i->FillArea);
+			File << "}}";
+		
+			if (i+1 != List.end())
+				File << ",\n";
+			else
+				File << "\n";
+		}
+		File << "}\n";
+	}
 }
 
 void MakePackedImage(
@@ -186,7 +302,7 @@ void MakePackedImage(
 	}
 	
 	PackImages(ImagePath, ImageList);
-	WriteDictionary(DictionaryFile, DictionaryName, ImageList);
+	WriteDictionary(DictionaryFile, DictionaryName, "NinePatches", ImageList);
 }
 
 int main(int argc, char* argv[])
@@ -202,10 +318,10 @@ int main(int argc, char* argv[])
 	std::vector<std::string> SourceList;
 	
 	desc.add_options()
-	("image-path", po::value<std::string>(&ImagePath)->default_value("packed_image.png"), "Set the path where to write the packed image data")
-	("dict-path", po::value<std::string>(&DictionaryPath)->default_value("packed_image.lua"), "Set the path where to write the dictionary for the image data")
-	("dict-name", po::value<std::string>(&DictionaryName)->default_value("Dictionary"), "Set the variable-name for the generated table")
-	("image", po::value<std::vector<std::string>>(&SourceList), "Individual paths or folders to use as sources")
+		("image-path", po::value<std::string>(&ImagePath)->default_value("packed_image.png"), "Set the path where to write the packed image data")
+		("dict-path", po::value<std::string>(&DictionaryPath)->default_value("packed_image.lua"), "Set the path where to write the dictionary for the image data")
+		("dict-name", po::value<std::string>(&DictionaryName)->default_value("Dictionary"), "Set the variable-name for the generated table")
+		("image", po::value<std::vector<std::string>>(&SourceList), "Individual paths or folders to use as sources")
 	;
 	
 	po::positional_options_description p;
